d06d879103b67bacc4fc52e374e48f175da65dde
me0321 <78087051+me0321@users.noreply.github.com>
2024-12-14 11:06:25 +0800

    Add files via upload

---
+++ dist/index.config.js
@@ -0,0 +1,492 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.config.js
+var index_config_exports = {};
+__export(index_config_exports, {
+  default: () => index_config_default
+});
+module.exports = __toCommonJS(index_config_exports);
+var index_config_default = {
+  kunyu77: {
+    testcfg: {
+      bbbb: "aaaaa"
+    }
+  },
+  ali: {
+    token: "",
+    token280: ""
+  },
+  quark: {
+    cookie: ""
+  },
+  uc: {
+    cookie: ""
+  },
+  xiaoya: {
+    url: "https://tvbox.omii.top/vod1/DixHtoGB"
+  },
+  yiso: {
+    url: "https://yiso.fun",
+    cookie: "__51vcke__JkIGvjjs25ETn0wz=279cdece-38ce-5256-b594-32f9f47e2bbd; __51vuft__JkIGvjjs25ETn0wz=1710318927617; satoken=49c9e954-78bd-4695-9954-ad3c1798ae6f; __51uvsct__JkIGvjjs25ETn0wz=36; __vtins__JkIGvjjs25ETn0wz=%7B%22sid%22%3A%20%22164c8224-7312-51a1-b9ec-2bbc8ee14ca4%22%2C%20%22vd%22%3A%202%2C%20%22stt%22%3A%20625365%2C%20%22dr%22%3A%20625365%2C%20%22expires%22%3A%201719035615461%2C%20%22ct%22%3A%201719033815461%7D; cf_clearance=3ot9BuJJP5EwIXD3Cpr7k5Jd7cuvvAWCO5io2oWIOkw-1719033817-1.0.1.1-gsYAae60t4jJ3vZTObvWxwH1H1NBcYiQ3PXYhWz5tITgYHYmTaX2jmkSvqIo5xP.VzyHJe327fQGc3rfUPGk4w"
+  },
+  bili: {
+    categories: "经典无损音乐合集#帕梅拉#太极拳#健身#舞蹈#音乐#歌曲#MV4K#演唱会4K#白噪音4K#知名UP主#说案#解说#演讲#时事#探索发现超清#纪录片超清#沙雕动画#沙雕穿越#沙雕#平面设计教学#软件教程#实用教程#旅游#风景4K#食谱#美食超清#搞笑#球星#动物世界超清#相声小品#戏曲#儿童#小姐姐4K#热门#旅行探险",
+    cookie: "buvid3=2EE7714E-396D-8B09-96FE-3D18B35CBD6E30391infoc; b_nut=1687691130; _uuid=6BEEEC104-35AF-A652-D66C-A61037EB9CCEF31032infoc; CURRENT_FNVAL=4048; rpdid=|(J~|~u~R||R0J'uY)~Ymm~~l; header_theme_version=CLOSE; fingerprint=979e0297d71b5da7d33048fd0f2105a6; buvid_fp_plain=undefined; buvid4=D599A569-8697-E141-B839-77A458D75A3432132-023062519-8ql5Wnsq%2BQ6f%2BEcmn2YT1A%3D%3D; enable_web_push=DISABLE; buvid_fp=979e0297d71b5da7d33048fd0f2105a6; hit-dyn-v2=1; home_feed_column=5; PVID=1; FEED_LIVE_VERSION=V_FAVOR_WATCH_LATER; browser_resolution=1599-821; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTA3NzMyNzAsImlhdCI6MTcxMDUxNDAxMCwicGx0IjotMX0.aGEuplXBVtg3SsfVeiC37aO4j-RBnnz67J4TzxtD3IA; bili_ticket_expires=1710773210; bp_video_offset_1679307957=909120323202318338; CURRENT_QUALITY=80; SESSDATA=0b9e2f11%2C1726068775%2Cbe3a3%2A32CjCp-4wRSorM2ubk-A5LfLd72V9iOeCfrTDWjUZmfmPKGxd4pKhcIKHbkJl4yXbibT8SVkF3TnhVSmtxb05ZU0N4Q05LdVA1MXdDX3ZHNDdrT1hkU3FDY3dHYUFEQURuc0w4U1RzanZ2RXJvOXVQaFRrMUV5VjJBVlJSWmI3VWo5aUx6eUczcDR3IIEC; bili_jct=21a583be26b77f6f990df8cd4563eb0d; DedeUserID=3546647141288212; DedeUserID__ckMd5=6d38879780c538a1; b_lsid=FA49D8A5_18E4C7DD168"
+  },
+  live: {
+    contents: "https://gitee.com/galnt/cctv/raw/master/contents.txt",
+    url: [
+      {
+        name: "肥羊 ",
+        url: "https://iptv.887979.xyz/tv.m3u"
+      },
+      {
+        name: "肥羊 ",
+        url: "https://live.wqwqwq.sbs/tv.m3u"
+      },
+      {
+        name: "港台 ",
+        url: "https://4g.wqwqwq.sbs/iptv.m3u"
+      },
+      {
+        name: "体育 ",
+        url: "https://tv.iill.top/m3u/Sport"
+      }
+    ]
+  },
+  wogg: {
+    url: "https://www.wogg.net"
+  },
+  tudou: {
+    url: "https://tudou.lvdoui.top"
+  },
+  wobg: {
+    url: "https://wobge.run.goorm.io/"
+  },
+  czzy: {
+    url: "https://cz01.vip"
+  },
+  hezi: {
+    url: "https://www.fygame.top/"
+  },
+  ttkx: {
+    url: "http://ttkx.live:7728/"
+  },
+  cm: {
+    url: "https://tv.yydsys.top"
+  },
+  libvio: {
+    url: "https://libvio.app/"
+  },
+  xxpan: {
+    url: "https://xpanpan.site"
+  },
+  appys: {
+    ttmja: [{
+      name: "天天美剧",
+      url: "https://www.ttmja.com/api.php/app/",
+      // categories: ['国产剧', '香港剧', '韩国剧', '欧美剧', '台湾剧', '日本剧', '海外剧', '泰国剧', '短剧', '动作片', '喜剧片', '爱情片', '科幻片', '恐怖片', '剧情片', '战争片', '动漫片', '大陆综艺', '港台综艺', '日韩综艺', '欧美综艺', '国产动漫', '日韩动漫', '欧美动漫', '港台动漫', '海外动漫', '记录片'],                        
+      search: true
+      //搜索开关 true开 false关
+    }],
+    netfly: [{
+      name: "奈飞",
+      url: "http://www.netfly.tv/api.php/app/",
+      // categories: ['国产剧', '香港剧', '韩国剧', '欧美剧', '台湾剧', '日本剧', '海外剧', '泰国剧', '短剧', '动作片', '喜剧片', '爱情片', '科幻片', '恐怖片', '剧情片', '战争片', '动漫片', '大陆综艺', '港台综艺', '日韩综艺', '欧美综艺', '国产动漫', '日韩动漫', '欧美动漫', '港台动漫', '海外动漫', '记录片'],                        
+      search: true
+      //搜索开关 true开 false关
+    }]
+  },
+  m3u8cj: {
+    ykm3u8: [{
+      name: "易看源",
+      url: "https://api.yikanapi.com/api.php/provide/vod/from/yikan",
+      categories: ["韩国伦理", "日本伦理", "香港伦理", "西方伦理", "台湾伦理", "日本写真", "西方写真", "国产写真", "动漫"],
+      search: true
+    }],
+    clm3u8: [{
+      name: "草榴源",
+      url: "https://www.caoliuzyw.com/api.php/provide/vod/from/clm3u8",
+      categories: ["国产传媒", "国产自拍", "女优合集", "中文字幕", "亚洲无码", "亚洲有码", "国产探花", "人妻熟女", "强奸乱伦", "网曝门事件", "绿帽淫妻", "欧美情色", "动漫卡通", "美女主播", "三级伦理", "人妻熟女", "AV解说", "国产乱伦"],
+      search: true
+    }],
+    askm3u8: [{
+      name: "奥卡源",
+      url: "https://aosikazy.com/api.php/provide/vod/",
+      search: true
+    }],
+    sngm3u8: [{
+      name: "南国源",
+      url: "https://api.sexnguon.com/api.php/provide/vod/",
+      search: true
+    }],
+    ptm3u8: [{
+      name: "葡萄源",
+      url: "https://caiji.putaozyw.net/inc/apijson_vod.php",
+      search: true
+    }],
+    ddm3u8: [{
+      name: "滴滴源",
+      url: "https://api.ddapi.cc/api.php/provide/vod/",
+      search: true
+    }],
+    swm3u8: [{
+      name: "丝袜源",
+      url: "https://www.siwazyw.tv/api.php/provide/vod/",
+      categories: ["亚洲无码", "亚洲有码", "欧美情色", "中文字幕", "动漫卡通", "美女主播", "人妻熟女", "强奸乱伦", "美女主播", "日韩伦理", "国产自拍", "精选口爆", "同性同志", "重口味", "91大神", "AV解说"],
+      search: true
+    }],
+    adm3u8: [{
+      name: "爱豆源",
+      url: "http://chujia.cc/api.php/provide/vod/from/m3u8",
+      categories: ["麻豆视频", "中文字幕", "日本无码", "童颜巨乳", "校园萝莉", "女优明星", "角色扮演", "制服诱惑", "强奸乱伦", "AI换脸", "黑料泄密", "主播直播", "国产精品", "探花视频", "SM调教", "人妻熟女"],
+      search: true
+    }]
+  },
+  alist: [
+    {
+      "name": "🟢 合集",
+      "server": "http://www.jczyl.top:5244/"
+    },
+    {
+      "name": "🟢 东哥",
+      "server": "http://101.34.67.237:5244/"
+    },
+    {
+      "name": "🟢 美云",
+      "server": "https://h.dfjx.ltd/"
+    },
+    {
+      "name": "🟢 小新",
+      "server": "https://pan.cdnxin.top/"
+    },
+    {
+      "name": "🟢 白云",
+      "server": "http://breadmyth.asuscomm.com:22222/"
+    },
+    {
+      "name": "🟢 小鸭",
+      "server": "http://www.214728327.xyz:5201/"
+    },
+    {
+      "name": "🟢 瑶瑶",
+      "server": "https://lyly.run.goorm.io/"
+    },
+    {
+      "name": "🟢 潇洒",
+      "server": "https://alist.azad.asia/"
+    },
+    {
+      "name": "🟢 鹏程",
+      "server": "https://pan.pengcheng.team/"
+    },
+    {
+      "name": "🟢 浅唱",
+      "server": "http://vtok.pp.ua/"
+    },
+    {
+      "name": "🟢 小丫",
+      "server": "http://alist.xiaoya.pro/"
+    },
+    {
+      "name": "🟢 触光",
+      "server": "https://pan.ichuguang.com"
+    },
+    {
+      "name": "🟢 弱水",
+      "server": "http://shicheng.wang:555/"
+    },
+    {
+      "name": "🟢 神器",
+      "server": "https://alist.ygxz.xyz/"
+    },
+    {
+      "name": "🟢 资源",
+      "server": "https://pan.ecve.cn/"
+    },
+    {
+      "name": "🟢 雨呢",
+      "server": "https://pan.clun.top/"
+    },
+    {
+      "name": "🟢 oeio",
+      "server": "https://o.oeio.repl.co/"
+    },
+    {
+      "name": "🟢 悦享",
+      "server": "https://nics.eu.org/"
+    },
+    {
+      "name": "🟢 分享",
+      "server": "https://ofoo.ml/"
+    },
+    {
+      "name": "🟢 PRO",
+      "server": "https://alist.prpr.run/"
+    },
+    {
+      "name": "🟢 多多",
+      "server": "https://pan.xwbeta.com"
+    },
+    {
+      "name": "🟢 小陈",
+      "server": "https://ypan.cc/"
+    },
+    {
+      "name": "🟢 只鱼",
+      "server": "https://alist.youte.ml"
+    },
+    {
+      "name": "🟢 七米",
+      "server": "https://al.chirmyram.com"
+    },
+    {
+      "name": "🟢 九帝",
+      "server": "https://alist.shenzjd.com"
+    },
+    {
+      "name": "🟢 白雪",
+      "server": "https://pan.jlbx.xyz"
+    },
+    {
+      "name": "🟢 星梦",
+      "server": "https://pan.bashroot.top"
+    },
+    {
+      "name": "🟢 repl",
+      "server": "https://ali.liucn.repl.co"
+    },
+    {
+      "name": "🟢 讯维",
+      "server": "https://pan.xwbeta.com"
+    }
+  ],
+  color: [
+    {
+      light: {
+        bg: "https://img.omii.top/i/2024/03/28/mexspg.webp",
+        bgMask: "0x50ffffff",
+        primary: "0xff446732",
+        onPrimary: "0xffffffff",
+        primaryContainer: "0xffc5efab",
+        onPrimaryContainer: "0xff072100",
+        secondary: "0xff55624c",
+        onSecondary: "0xffffffff",
+        secondaryContainer: "0xffd9e7cb",
+        onSecondaryContainer: "0xff131f0d",
+        tertiary: "0xff386666",
+        onTertiary: "0xffffffff",
+        tertiaryContainer: "0xffbbebec",
+        onTertiaryContainer: "0xff002020",
+        error: "0xffba1a1a",
+        onError: "0xffffffff",
+        errorContainer: "0xffffdad6",
+        onErrorContainer: "0xff410002",
+        background: "0xfff8faf0",
+        onBackground: "0xff191d16",
+        surface: "0xfff8faf0",
+        onSurface: "0xff191d16",
+        surfaceVariant: "0xffe0e4d6",
+        onSurfaceVariant: "0xff191d16",
+        inverseSurface: "0xff2e312b",
+        inverseOnSurface: "0xfff0f2e7",
+        outline: "0xff74796d",
+        outlineVariant: "0xffc3c8bb",
+        shadow: "0xff000000",
+        scrim: "0xff000000",
+        inversePrimary: "0xffaad291",
+        surfaceTint: "0xff446732"
+      },
+      dark: {
+        bg: "https://img.omii.top/i/2024/03/28/mexyit.webp",
+        bgMask: "0x50000000",
+        primary: "0xffaad291",
+        onPrimary: "0xff173807",
+        primaryContainer: "0xff2d4f1c",
+        onPrimaryContainer: "0xffc5efab",
+        secondary: "0xffbdcbb0",
+        onSecondary: "0xff283420",
+        secondaryContainer: "0xff3e4a35",
+        onSecondaryContainer: "0xffd9e7cb",
+        tertiary: "0xffa0cfcf",
+        onTertiary: "0xff003738",
+        tertiaryContainer: "0xff1e4e4e",
+        onTertiaryContainer: "0xffbbebec",
+        error: "0xffffb4ab",
+        onError: "0xff690005",
+        errorContainer: "0xff93000a",
+        onErrorContainer: "0xffffdad6",
+        background: "0xff11140e",
+        onBackground: "0xffe1e4d9",
+        surface: "0xff11140e",
+        onSurface: "0xffe1e4d9",
+        surfaceVariant: "0xff43483e",
+        onSurfaceVariant: "0xffe1e4d9",
+        inverseSurface: "0xffe1e4d9",
+        inverseOnSurface: "0xff2e312b",
+        outline: "0xff8d9286",
+        outlineVariant: "0xff43483e",
+        shadow: "0xff000000",
+        scrim: "0xff000000",
+        inversePrimary: "0xff446732",
+        surfaceTint: "0xffaad291"
+      }
+    },
+    {
+      light: {
+        "bg": "https://img.omii.top/i/2024/03/27/oudroy-0.webp",
+        "bgMask": "0x50ffffff",
+        "primary": "0xFFA00B0B",
+        "onPrimary": "0xFFFFFFFF",
+        "primaryContainer": "0xFF333433",
+        "onPrimaryContainer": "0xFFBDC0B0",
+        "secondary": "0xFF55624C",
+        "onSecondary": "0xFFFFFFFF",
+        "secondaryContainer": "0xFFFFEBEE",
+        "onSecondaryContainer": "0xFFeb4d4b",
+        "tertiary": "0xFF663840",
+        "onTertiary": "0xFFFFFFFF",
+        "tertiaryContainer": "0xFFEBBBBE",
+        "onTertiaryContainer": "0xFF200006",
+        "error": "0xFFBA1A1A",
+        "onError": "0xFFFFFFFF",
+        "errorContainer": "0xFFFFDAD6",
+        "onErrorContainer": "0xFF410002",
+        "background": "0xFFFDFDF5",
+        "onBackground": "0xFFB94242",
+        "surface": "0xFFFDFDF5",
+        "onSurface": "0xFFB94242",
+        "surfaceVariant": "0xFFE4D6D8",
+        "onSurfaceVariant": "0xFFB94242",
+        "inverseSurface": "0xFF312C2C",
+        "onInverseSurface": "0xFFF1F1EA",
+        "outline": "0xFF74796D",
+        "outlineVariant": "0xFFC3C8BB",
+        "shadow": "0xFF000000",
+        "scrim": "0xFF000000",
+        "inversePrimary": "0xFFff7979",
+        "surfaceTint": "0xFFA00B0B"
+      },
+      dark: {
+        "bg": "https://img.omii.top/i/2024/01/25/xdiepq-0.webp",
+        "bgMask": "0x50000000",
+        "primary": "0xFFff7979",
+        "onPrimary": "0xFFA00B0B",
+        "primaryContainer": "0xFFeb4d4b",
+        "onPrimaryContainer": "0xFFFFCDD2",
+        "secondary": "0xFFBDCBAF",
+        "onSecondary": "0xFF342023",
+        "secondaryContainer": "0xFF4A3536",
+        "onSecondaryContainer": "0xFFE7CACE",
+        "tertiary": "0xFFA0CFCF",
+        "onTertiary": "0xFF003737",
+        "tertiaryContainer": "0xFF1E4E4E",
+        "onTertiaryContainer": "0xFFBBEBEB",
+        "error": "0xFFFFB4AB",
+        "errorContainer": "0xFF93000A",
+        "onError": "0xFF690005",
+        "onErrorContainer": "0xFFFFDAD6",
+        "background": "0xFF1C1818",
+        "onBackground": "0xFFE3E3DC",
+        "outline": "0xFF92868B",
+        "onInverseSurface": "0xFF1C1818",
+        "inverseSurface": "0xFFE3DCE1",
+        "inversePrimary": "0xFFeb4d4b",
+        "shadow": "0xFF000000",
+        "surfaceTint": "0xFFDA607D",
+        "outlineVariant": "0xFF483E41",
+        "scrim": "0xFF000000",
+        "surface": "0xFF1C1818",
+        "onSurface": "0xFFC7C7C0",
+        "surfaceVariant": "0xFF43483E",
+        "onSurfaceVariant": "0xFFC7C7C0"
+      }
+    },
+    {
+      light: {
+        bg: "",
+        bgMask: "0x50ffffff",
+        primary: "0xFF2B6C00",
+        onPrimary: "0xFFFFFFFF",
+        primaryContainer: "0xFFA6F779",
+        onPrimaryContainer: "0xFF082100",
+        secondary: "0xFF55624C",
+        onSecondary: "0xFFFFFFFF",
+        secondaryContainer: "0xFFD9E7CA",
+        onSecondaryContainer: "0xFF131F0D",
+        tertiary: "0xFF386666",
+        onTertiary: "0xFFFFFFFF",
+        tertiaryContainer: "0xFFBBEBEB",
+        onTertiaryContainer: "0xFF002020",
+        error: "0xFFBA1A1A",
+        onError: "0xFFFFFFFF",
+        errorContainer: "0xFFFFDAD6",
+        onErrorContainer: "0xFF410002",
+        background: "0xFFFDFDF5",
+        onBackground: "0xFF1A1C18",
+        surface: "0xFFFDFDF5",
+        onSurface: "0xFF1A1C18",
+        surfaceVariant: "0xFFE0E4D6",
+        onSurfaceVariant: "0xFF1A1C18",
+        inverseSurface: "0xFF2F312C",
+        onInverseSurface: "0xFFF1F1EA",
+        outline: "0xFF74796D",
+        outlineVariant: "0xFFC3C8BB",
+        shadow: "0xFF000000",
+        scrim: "0xFF000000",
+        inversePrimary: "0xFF8CDA60",
+        surfaceTint: "0xFF2B6C00"
+      },
+      dark: {
+        bg: "",
+        bgMask: "0x50000000",
+        primary: "0xFF8CDA60",
+        onPrimary: "0xFF133800",
+        primaryContainer: "0xFF1F5100",
+        onPrimaryContainer: "0xFFA6F779",
+        secondary: "0xFFBDCBAF",
+        onSecondary: "0xFF283420",
+        secondaryContainer: "0xFF3E4A35",
+        onSecondaryContainer: "0xFFD9E7CA",
+        tertiary: "0xFFA0CFCF",
+        onTertiary: "0xFF003737",
+        tertiaryContainer: "0xFF1E4E4E",
+        onTertiaryContainer: "0xFFBBEBEB",
+        error: "0xFFFFB4AB",
+        errorContainer: "0xFF93000A",
+        onError: "0xFF690005",
+        onErrorContainer: "0xFFFFDAD6",
+        background: "0xFF1A1C18",
+        onBackground: "0xFFE3E3DC",
+        outline: "0xFF8D9286",
+        onInverseSurface: "0xFF1A1C18",
+        inverseSurface: "0xFFE3E3DC",
+        inversePrimary: "0xFF2B6C00",
+        shadow: "0xFF000000",
+        surfaceTint: "0xFF8CDA60",
+        outlineVariant: "0xFF43483E",
+        scrim: "0xFF000000",
+        surface: "0xFF1A1C18",
+        onSurface: "0xFFC7C7C0",
+        surfaceVariant: "0xFF43483E",
+        onSurfaceVariant: "0xFFC7C7C0"
+      }
+    }
+  ]
+};
